a200f966c8276bd62d46b91b927c3f5bde7f6adc
upos <dgv7@daum.net>
2024-07-04 11:09:05 +0900

    Initial Commit

---
+++ SelfCheckIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfCheckIn
+//
+//  Created by 김동건 on 7/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
